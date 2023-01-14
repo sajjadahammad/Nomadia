7ba0436ea1b742f4e8ae6afb72ca75cc6b8ea13a
sajjadahammed <sajjad08@protonmail.com>
2023-01-14 09:51:17 +0530

    14-1-2023

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import Welcome from '../components/welcome/Welcome'
-import Header from '../components/navbar/Header'
-import Footer from '../components/footer/Footer'
-import Destination from '../components/destination/Destination'
-import Testimontial from '../components/testimonial/Testimonial'
-
+import React from "react";
+import Welcome from "../components/welcome/Welcome";
+import Header from "../components/navbar/Header";
+import Footer from "../components/footer/Footer";
+import Destination from "../components/destination/Destination";
+import Testimontial from "../components/testimonial/Testimonial";
 
 function Home() {
   return (
     <div>
-     <Header/>
-      <Welcome/>
-      <div style={{'marginTop':'35px'}}></div>
-      <Destination/>
-      <div style={{'marginTop':'35px'}}></div>
-      <Testimontial/>
-      <div style={{'marginTop':'35px'}}></div>
-      <Footer/>
+      <Header />
+      <Welcome />
+      <div style={{ marginTop: "35px" }}></div>
+      <Destination />
+      <div style={{ marginTop: "35px" }}></div>
+      <Testimontial />
+      <div style={{ marginTop: "35px" }}></div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
